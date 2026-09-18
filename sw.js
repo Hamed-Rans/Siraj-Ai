f95358d035439da0816d85d7a4b1d02e702dfb55
Hamed Ansarifar <hamedansarifar@gmail.com>
2026-09-19 02:22:16 +0330

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE = 'siraj-v3.0';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './style-v2.css',
+  './script.js',
+  './planner-v2.js',
+  './siraj-logo.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  if (e.request.url.indexOf('siraj-proxy') > -1) return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (res.ok && res.type === 'basic') {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
+
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(list => {
+      for (const c of list) if (c.url.indexOf(self.location.origin) === 0 && 'focus' in c) return c.focus();
+      if (clients.openWindow) return clients.openWindow('./');
+    })
+  );
+});
