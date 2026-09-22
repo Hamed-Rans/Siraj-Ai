d26748fa6960d18f2098c3712bd00f8656b78706
Hamed Ansarifar <hamedansarifar@gmail.com>
2026-09-22 17:06:30 +0330

    Update script.js

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ const APP_CONFIG = {
         appVersion: '2.5'
     },
     themeColors: ['navy', 'crimson', 'gold', 'purple', 'emerald', 'indigo'],
-    colorNames: { navy: 'شبانه', crimson: 'آتشین', gold: 'زرین', purple: 'جادویی', emerald: 'طبیعی', indigo: 'نیلی' },
+    colorNames: { navy: 'نیلگون', crimson: 'ارغوان', gold: 'زرین', purple: 'بنفشه', emerald: 'زمرد', indigo: 'نیلی' },
     colorIcons: {
         navy: '<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/>',
         crimson: '<path d="M8.5 14.5A2.5 2.5 0 0 0 11 12c0-1.38-.5-2-1-3-1.072-2.143-.224-4.054 2-6 .5 2.5 2 4.9 4 6.5 2 1.6 3 3.5 3 5.5a7 7 0 1 1-14 0c0-1.153.433-2.294 1-3a2.5 2.5 0 0 0 2.5 2.5z"/>',
